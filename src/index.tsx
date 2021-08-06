839490b9880c0cc1c5316c33d0e5dd2e23aacd76
Helder Oliveira <heldrida@gmail.com>
2021-08-06 12:27:24 +0100

    refactor: 💡 takes any collection as a json instead, to speed up the dev experience

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
 import Icomoon, { IconSet } from './icomoon';
-import Plug from './collections/plug.json';
-
-type CollectionName = 'Plug';
 
 export const getIconList = (iconSet: IconSet) => {
   if (!iconSet || !Array.isArray(iconSet.icons)) return [];
   return iconSet.icons.map((icon) => icon.properties.name);
 };
 
-// eslint-disable-next-line consistent-return
-const getIconSet = (collection: string) => {
-  if (collection === 'Plug') {
-    return (Plug as IconSet);
-  }
-};
-
 export interface FleekonProps {
   className?: string,
-  collectionName: CollectionName,
+  collection: IconSet,
   color: string,
   icon: string,
   size: string,
@@ -26,25 +16,20 @@ export interface FleekonProps {
 
 const Fleekon = ({
   className,
-  collectionName,
+  collection,
   color,
   icon,
   size,
-}: FleekonProps) => {
-  const iconSet = getIconSet(collectionName);
-
-  return (iconSet
+}: FleekonProps) => (collection
     && (
       <Icomoon
-        iconSet={iconSet}
+        iconSet={collection}
         icon={icon}
         color={color}
         size={size}
         className={className}
       />
     )
-  )
-  || <>Icon missing!</>;
-};
+) || <>Icon missing!</>;
 
 export default Fleekon;
